1e67519656a77a55ccc125727a0caa448d8cba7f
Lillymar <lillymar.diaz@gmail.com>
2022-11-21 09:58:52 -0500

    Added Results

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,12 +1,14 @@
 import React, {useState} from "react";
 import axios from "axios";
+import Results from "./Results"
 import "./Dictionary.css";
 
 export default function Dictionary(){
     let [keyword, setKeyword] = useState("");
+    let [results, setResults]= useState(null);
 
 function handleResponse(response){
-    console.log(response.data[0]);
+    setResults(response.data[0]);
 }
 
     function search(event){
@@ -18,8 +20,6 @@ function handleResponse(response){
     axios.get(apiUrl).then(handleResponse)
     }
     
-
-
     function handleKeywordChange(event){
         setKeyword (event.target.value);
     }
@@ -28,6 +28,7 @@ function handleResponse(response){
     <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
     </form>
+    <Results results={results} />
       </div>
     );
 }

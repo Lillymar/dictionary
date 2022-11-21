9b3202fb1bb1f54873d91a96b7c7d3171282c65d
Lillymar <lillymar.diaz@gmail.com>
2022-11-20 19:03:59 -0500

    added Dictionary component

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Dictionary from "./Dictionary"
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+      <header className="App-header">
+       <h3> Dictionary</h3>
+       
+</header>
+<main>
+  <Dictionary/>
+</main>
+<footer className='text-center'> Coded by Lilly</footer>
+</div>
+    </div>
+  );
+}
+
+export default App;
